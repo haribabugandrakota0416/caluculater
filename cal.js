dd17eb98d4081e09c8a9c87aafacad533e5ff5ad
haribabugandrakota0416 <138872143+haribabugandrakota0416@users.noreply.github.com>
2023-07-10 17:15:56 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,96 @@
+/*var clickSound=new Audio()
+clickSound.src="./click.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}*/
+var clickSound1=new Audio()
+clickSound1.src="./brain-damage-148577.mp3"
+function clickAudio1(){
+    clickSound1.play()
+}
+var clickSound2=new Audio()
+clickSound2.src="./music1.mp3"
+function clickAudio2(){
+    clickSound2.play()
+}
+var clickSound3=new Audio()
+clickSound3.src="./music2.mp3"
+function clickAudio3(){
+    clickSound3.play()
+}
+var clickSound4=new Audio()
+clickSound4.src="./music3.mp3"
+function clickAudio4(){
+    clickSound4.play()
+}
+var clickSound5=new Audio()
+clickSound5.src="./music4.mp3"
+function clickAudio5(){
+    clickSound5.play()
+}
+var clickSound6=new Audio()
+clickSound6.src="./music5.mp3"
+function clickAudio6(){
+    clickSound6.play()
+}
+var clickSound7=new Audio()
+clickSound7.src="./music6.mp3"
+function clickAudio7(){
+    clickSound7.play()
+}
+var clickSound8=new Audio()
+clickSound8.src="./music7.mp3"
+function clickAudio8(){
+    clickSound8.play()
+}
+var clickSound9=new Audio()
+clickSound9.src="./music8.mp3"
+function clickAudio9(){
+    clickSound9.play()
+}
+var clickSound10=new Audio()
+clickSound10.src="./music9.mp3"
+function clickAudio10(){
+    clickSound10.play()
+}
+var clickSound11=new Audio()
+clickSound11.src="./music10.mp3"
+function clickAudio11(){
+    clickSound11.play()
+}
+var clickSound12=new Audio()
+clickSound12.src="./music11.mp3"
+function clickAudio12(){
+    clickSound12.play()
+}
+var clickSound13=new Audio()
+clickSound13.src="./music12.mp3"
+function clickAudio13(){
+    clickSound13.play()
+}
+var clickSound14=new Audio()
+clickSound14.src="./music13.mp3"
+function clickAudio14(){
+    clickSound14.play()
+}
+var clickSound15=new Audio()
+clickSound15.src="./music14.mp3"
+function clickAudio15(){
+    clickSound15.play()
+}
+var clickSound16=new Audio()
+clickSound16.src="./music15.mp3"
+function clickAudio16(){
+    clickSound16.play()
+}
+var clickSound17=new Audio()
+clickSound17.src="./music16.mp3"
+function clickAudio17(){
+    clickSound17.play()
+}
+var clickSound18=new Audio()
+clickSound18.src="./music17.mp3"
+function clickAudio18(){
+    clickSound18.play()
+}
